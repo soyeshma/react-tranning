b2c3dc3c46a4f727bfd41a7507d725c8bc5d30cc
soyeshma <kayasthasoyeshma7@gmail.com>
2022-11-12 09:18:27 +0545

    deleted tic-tac-toe related code

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-import Game from "./components/Game";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <div className="App">
-     <Game />
-
+     
     </div>
   )
 }
