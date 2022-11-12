cdebd996293fb128b233ffacbf7b2fdb75cf2763
soyeshma <kayasthasoyeshma7@gmail.com>
2022-11-12 08:32:31 +0545

    built tic tac toe

---
+++ src/components/square.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Square(props){
+  
+    
+    return (
+        <button className="square" onClick={props.onClick}>
+          {props.value}
+        </button>
+      );
+}
